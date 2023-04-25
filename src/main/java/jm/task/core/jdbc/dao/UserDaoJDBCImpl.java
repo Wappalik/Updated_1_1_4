22ab4832627ae3d8778c5b7bb6ad95911cd1a54e
Wappalik <baxriddinov_97@mail.ru>
2023-04-26 00:37:27 +0300

    осталось доделать getAllUserss

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,71 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
-
     }
 
     public void createUsersTable() {
-
+        try(Connection connection = Util.getConnection(); Statement statement = connection.createStatement()) {
+            statement.execute("CREATE TABLE IF NOT EXISTS chmoshniki (id BIGINT(10) PRIMARY KEY NOT NULL AUTO_INCREMENT," +
+                    "name VARCHAR (64), lastname VARCHAR (64), age TINYINT (10))");
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try(Connection connection = Util.getConnection(); Statement statement = connection.createStatement()) {
+            statement.execute("DROP TABLE IF EXISTS chmoshniki");
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+//        try(Connection connection = Util.getConnection(); PreparedStatement statement =
+//                connection.prepareStatement("INSERT INTO Table1 ()")) {
+//
+//        } catch (SQLException e) {
+//            e.getStackTrace();
+//        }
+        String saveUser = "INSERT INTO chmoshniki (name,lastname,age) VALUES ('" + name + "', '" + lastName + "', '" + String.valueOf(age) + "')";
+        try(Connection connection = Util.getConnection(); Statement statement = connection.createStatement()) {
+            statement.executeUpdate(saveUser);
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
+        try(Connection connection = Util.getConnection(); Statement statement = connection.createStatement();) {
+            statement.executeUpdate("DELETE FROM chmoshniki WHERE 'id' = " + id );
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
 
     }
 
     public List<User> getAllUsers() {
+    List <User> list = new ArrayList<>();
         return null;
     }
 
-    public void cleanUsersTable() {
 
+    public void cleanUsersTable() {
+    try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement()){
+        statement.executeUpdate("DELETE FROM chmoshniki");
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
+    }
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,24 +1,26 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
     public void createUsersTable() {
-
+    new UserDaoJDBCImpl().createUsersTable();
     }
 
     public void dropUsersTable() {
-
+    new UserDaoJDBCImpl().dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        new UserDaoJDBCImpl().saveUser(name,lastName,age);
+        System.out.println("User с именем – " + name  + " добавлен в базу данных");
     }
 
     public void removeUserById(long id) {
-
+        new UserDaoJDBCImpl().removeUserById(id);
     }
 
     public List<User> getAllUsers() {
@@ -26,6 +28,6 @@ public class UserServiceImpl implements UserService {
     }
 
     public void cleanUsersTable() {
-
+    new UserDaoJDBCImpl().cleanUsersTable();
     }
 }
